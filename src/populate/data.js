17f3b9c40260166e27eeec4b92201ebaf650d6a7
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-09-22 16:13:48 +0900

    for population

--- src/populate/data.js
+++ src/populate/data.js
@@ -1,65 +1,67 @@
 const data = {
-  portfolios: [
-    {
-      title: 'Job in Netcentric',
-      company: 'Netcentric',
-      companyWebsite: 'www.google.com',
-      location: 'Spain, Barcelona',
-      jobTitle: 'Engineer',
-      description: 'Doing something, programing....',
-      userId: 'google-oauth2|112908182107616195951',
-      startDate: '01/01/2014',
-      endDate: '01/01/2016'
-    },
-    {
-      title: 'Job in Siemens',
-      company: 'Siemens',
-      companyWebsite: 'www.google.com',
-      location: 'Korea, Seoul',
-      jobTitle: 'Software Engineer',
-      description: 'Responsoble for parsing framework for JSON medical data.',
-      userId: 'google-oauth2|112908182107616195951',
-      startDate: '01/01/2011',
-      endDate: '01/01/2013'
-    },
-    {
-      title: 'Work in USA',
-      company: 'WhoKnows',
-      companyWebsite: 'www.google.com',
-      location: 'USA, Montana',
-      jobTitle: 'Housekeeping',
-      description: 'So much responsibility....Overloaaaaaad',
-      userId: 'google-oauth2|112908182107616195951',
-      startDate: '01/01/2010',
-      endDate: '01/01/2011'
-    }
-  ],
-  blogs: [
-    {
-      slug: 'my-first-blog',
-      title: 'My First Blog',
-      subTitle: 'It was chilly winter day...',
-      content: '<p>Some very nice content</p>',
-      userId: 'google-oauth2|112908182107616195951',
-      status: 'published'
-    },
-    {
-      slug: 'my-second-blog',
-      title: 'My Second Blog',
-      subTitle: 'It was hot summer day...',
-      content: '<p>Some very nice content</p>',
-      userId: 'google-oauth2|112908182107616195951',
-      status: 'published'
-    },
-    {
-      slug: 'my-third-blog',
-      title: 'My Third Blog',
-      subTitle: 'It was rainy spring day...',
-      content: '<p>Some very nice content</p>',
-      userId: 'google-oauth2|112908182107616195951',
-      status: 'published'
-    }
-  ]
-}
+	portfolios: [
+		{
+			title: 'Petpairs',
+			company: 'CodeStates',
+			companyWebsite: 'https://github.com/Gimoring/Petpairs',
+			location: 'Home, Korea',
+			jobTitle: 'Frontend / 보일러 플레이트 구축 및 메인페이지 담당.',
+			description:
+				'Tinder 사이트에서 아이디어를 떠올렸으며, 사람이 아닌 애완동물에게 적용해 사용자에게 좋은 친구와 애완동물에게도 좋은 친구를 만들어주면 좋을 것 같다는 생각을하여 진행헀던 프로젝트입니다.',
+			userId: 'google-oauth2|112908182107616195951',
+			startDate: '06/06/2021',
+			endDate: '07/07/2021',
+		},
+		{
+			title: 'Joiner',
+			company: 'CodeStates',
+			companyWebsite: 'https://github.com/codestates/Joiner-client',
+			location: 'Home, Korea',
+			jobTitle: 'Frontend / 보일러 플레이트 구축 및 랜딩, 메인페이지 담당',
+			description:
+				'워라벨을 중요시하는 요즘, 한 눈에 원하는 그룹을 선택하여 활동할 수 있게 구현하였던 프로젝트입니다.',
+			userId: 'google-oauth2|112908182107616195951',
+			startDate: '05/05/2021',
+			endDate: '06/06/2021',
+		},
+		{
+			title: 'Work in USA',
+			company: 'WhoKnows',
+			companyWebsite: 'www.google.com',
+			location: 'USA, Montana',
+			jobTitle: 'Housekeeping',
+			description: 'So much responsibility....Overloaaaaaad',
+			userId: 'google-oauth2|112908182107616195951',
+			startDate: '01/01/2010',
+			endDate: '01/01/2011',
+		},
+	],
+	blogs: [
+		{
+			slug: 'my-first-blog',
+			title: 'My First Blog',
+			subTitle: 'It was chilly winter day...',
+			content: '<p>Some very nice content</p>',
+			userId: 'google-oauth2|112908182107616195951',
+			status: 'published',
+		},
+		{
+			slug: 'my-second-blog',
+			title: 'My Second Blog',
+			subTitle: 'It was hot summer day...',
+			content: '<p>Some very nice content</p>',
+			userId: 'google-oauth2|112908182107616195951',
+			status: 'published',
+		},
+		{
+			slug: 'my-third-blog',
+			title: 'My Third Blog',
+			subTitle: 'It was rainy spring day...',
+			content: '<p>Some very nice content</p>',
+			userId: 'google-oauth2|112908182107616195951',
+			status: 'published',
+		},
+	],
+};
 
 module.exports = data;
